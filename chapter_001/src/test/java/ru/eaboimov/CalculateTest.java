d976de5d442814872729ffa33cec2ff3be2619ad
EndOne13 <tuz1k@mail.ru>
2017-04-22 18:08:54 +0300

    Test

--- chapter_001/src/test/java/ru/eaboimov/CalculateTest.java
+++ chapter_001/src/test/java/ru/eaboimov/CalculateTest.java
@@ -1,10 +1,24 @@
 package ru.eaboimov;
 
 import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
+/**
+*Test
+*
+*@author Evgenyi Aboimov (mailto:tuz1k@mail.ru)
+*@version $Id$
+*@since 0.1
+*/
+
 public class CalculateTest {
+	
+	/**
+	*Test add.
+	*/
 	
 	@Test
 	public void whenSetStopInEchoThenReturnThreeStops() {
