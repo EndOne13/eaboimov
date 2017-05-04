15fa3c1d532d438f40bf61c7dcdfbaad148ebb2c
EndOne13 <tuz1k@mail.ru>
2017-05-04 22:09:29 +0300

    Point

---
+++ chapter_001/src/main/java/ru/eaboimov/Point.java
@@ -0,0 +1,22 @@
+package ru.eaboimov.condition;
+ 
+public class Point {
+   private int x;
+   private int y;
+ 
+   public  Point(int x, int y) {
+     this.x = x;
+     this.y = y;
+  }
+  
+  public int getX() {
+     return this.x;
+  }
+ 
+  public int getY() {
+     return this.y;
+  }
+  public boolean is(int a, int b){
+     return y == a * x + b;
+  }
+}
